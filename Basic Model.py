94ffc2fd477baf49a903af1c4194804846182bf5
Debanik Banerjee <banerjeedebanik@gmail.com>
2019-06-20 16:04:11 +0530

    Basic Model = LeEnt 5

--- Basic Model.py
+++ Basic Model.py
@@ -3,11 +3,11 @@ from PIL import Image
 from search_videos_in_directory import search_videos
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# input_direc = '/home/debanik/downloaded_videos'
-# output_direc = '/home/debanik/PycharmProjects/Detecting-Deepfakes/temp_n'
-
-input_direc = input("Enter the absolute path of the input directory: ")
-output_direc = input("Enter the absolute path of the output directory: ")
+input_direc = '/home/debanik/downloaded_videos'
+output_direc = '/home/debanik/PycharmProjects/Detecting-Deepfakes/Face_images_100'
+#
+# input_direc = input("Enter the absolute path of the input directory: ")
+# output_direc = input("Enter the absolute path of the output directory: ")
 search_videos(input_direc, output_direc)
 
 
@@ -29,29 +29,28 @@ datagen = ImageDataGenerator(samplewise_center=True,
 # prepare an iterators for each dataset
 training_images = datagen.flow_from_directory(output_direc, class_mode='binary', batch_size=64, subset='training')
 validation_images = datagen.flow_from_directory(output_direc, class_mode='binary', batch_size=64, subset='validation')
-# # confirm the iterator works
-# batchX, batchy = train_it.next()
-# print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
 
-
+import tensorflow.keras.layers as layers
 from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, Sequential
 
 
-x = Input(shape = (256, 256, 3))
+model = Sequential()
+model.add(layers.Input(shape = (256, 256, 3)))
 
-x1 = Conv2D(160, (3, 3), strides = 1, padding='same', activation = 'relu')(x)
-x1 = Conv2D(40, (1, 1), padding='same', activation = 'relu')(x1)
-x1 = BatchNormalization()(x1)
-x1 = MaxPooling2D(pool_size=(8, 8), padding='same')(x1)
+model.add(layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(32,32,1)))
+model.add(layers.AveragePooling2D())
 
-y = Flatten()(x1)
-y = Dropout(0.5)(y)
-y = Dense(100, activation='relu')(y)
-y = Dense(1, activation='sigmoid')(y)
+model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
+model.add(layers.AveragePooling2D())
 
+model.add(layers.Flatten())
 
-model = Model(inputs = x, outputs = y)
+model.add(layers.Dense(units=120, activation='relu'))
+
+model.add(layers.Dense(units=84, activation='relu'))
+
+model.add(layers.Dense(units=1, activation = 'sigmoid'))
 
 model.summary()
 
